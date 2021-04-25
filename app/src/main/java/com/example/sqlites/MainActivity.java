71a4b2c7934ac26fb8f53bb0ba739d8197f03759
salis2 <56523603+salis2@users.noreply.github.com>
2021-04-25 15:37:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sqlites/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.sqlites;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.sqlites.adapter.TemanAdapter;
+import com.example.sqlites.database.BDController;
+import com.example.sqlites.database.Teman;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private TemanAdapter adapter;
+    private ArrayList<Teman> temanArrayList;
+    BDController controller = new BDController(this);
+    String id,nm,tlp;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycleView);
+        fab = findViewById(R.id.floatingBtn);
+        BacaData();
+        adapter = new TemanAdapter(temanArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TedmanBaku.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void BacaData() {
+        ArrayList<HashMap<String, String>> daftarTeman = controller.getAllTeman();
+        temanArrayList = new ArrayList<>();
+        //memindah dari hasil query kedalam teman
+        for (int i=0;i<daftarTeman.size();i ++){
+            Teman teman = new Teman();
+
+            teman.setId(daftarTeman.get(i).get("id").toString());
+            teman.setNama(daftarTeman.get(i).get("nama").toString());
+            teman.setTelpon(daftarTeman.get(i).get("telpon").toString());
+            //Pindah dari Teman kedalam ArrayList teman di Adapter
+            temanArrayList.add(teman);
+        }
+    }
+}
